1e2d9d44e9ab40b0041b4b140678780b3e552039
BenAvrahamos <benavraham1998@gmail.com>
2024-06-06 23:36:52 +0300

    Added layout for phone only header

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
 import { NavBar } from './cmps/NavBar'
 import { PostIndex } from './views/PostIndex'
+import { HeaderNav } from './cmps/HeaderNav'
 
 
 function App() {
 
   return <section>
+    <HeaderNav/>
     <NavBar />
     {/* <PostIndex /> */}
   </section>
---
+++ src/cmps/HeaderNav.jsx
@@ -0,0 +1,27 @@
+export function HeaderNav() {
+    return <section className="header-nav">
+
+        <img className="logo" src="src\assets\svgs\IntagramLogo.svg" alt="" />
+
+        <div>
+
+            <div className="imput-container">
+                <img src="src\assets\svgs\Search.svg" alt="" />
+                <div>search</div>
+            <input type="text" />
+            <div>X</div>
+            </div>
+
+          
+
+            <div>
+                <img className="notifications" src="src\assets\svgs\heart.svg" alt="" />
+            </div>
+
+        </div>
+
+
+
+
+    </section>
+}
