63bcbaf8d36ba503491f9e8029bee6755e88c34c
BenAvrahamos <benavraham1998@gmail.com>
2024-07-28 14:49:09 +0300

    Turned backdrop into it's own cmp

---
+++ src/cmps/BackDrop.jsx
@@ -0,0 +1,34 @@
+import { useEffect, useState, useRef } from 'react';
+
+export function BackDrop({children,disableAt = 0,zIndex,dataState }){
+
+    const [isBackdropDisabled, setIsBackdropDisabled] = useState(false)
+
+    useEffect(() => {
+        const handleResize = () => {
+            if (disableAt !== 0 && window.innerWidth <= disableAt) {
+                setIsBackdropDisabled(true);
+            } else {
+                setIsBackdropDisabled(false);
+            }
+        }
+
+        handleResize()
+        window.addEventListener('resize', handleResize);
+
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        }
+    }, [])
+
+    function handleBackdropClick(event) {
+
+        if (event.target === event.currentTarget && !isBackdropDisabled) {
+            dataState(null)
+        }
+    }
+
+return <section onClick={handleBackdropClick} className="backdrop" style={{ zIndex }}>
+    {children}
+</section>
+}
