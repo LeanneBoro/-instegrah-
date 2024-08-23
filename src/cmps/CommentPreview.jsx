d56d8d33884171f3baa8a944ba5c4c132e824de8
BenAvrahamos <benavraham1998@gmail.com>
2024-08-23 06:29:34 +0300

    Added comment placeholder when comment is loading

--- src/cmps/CommentPreview.jsx
+++ src/cmps/CommentPreview.jsx
@@ -1,41 +1,47 @@
+import { postService } from "../services/post.local.service"
+import { utilService } from "../services/util.service";
+import { CommentPreviewPlaceholder } from "./CommentPreviewPlaceholder"
+import { ProfileImg } from "./ProfileImg"
 
-import { ProfileImg } from "./ProfileImg";
+export function CommentPreview({ setModalData, comment, navigateToProfile,isCommentLoading }) {
 
+    if (isCommentLoading) return <CommentPreviewPlaceholder />
+    console.log(comment);
+    
 
-export function CommentPreview({ setModalData, comment, navigateToProfile }) {
+    return (
+        <section className="comment-preview flex">
+            <div onClick={() => navigateToProfile(comment.by.id)} className='cursor-pointer'>
+                <ProfileImg imgUrl={comment.by.profileImg} diameter={'35px'} />
+            </div>
 
-
-    return <section className="comment-preview flex">
-
-        <div onClick={() => navigateToProfile(comment.by.id)} className='cursor-pointer'>
-            <ProfileImg imgUrl={comment.commenter.profileImg} diameter={'35px'} />
-        </div>
-
-
-        <section className="comment">
-
-            <section className="content">
-                <h2 onClick={() => navigateToProfile(comment.by)} className="username cursor-pointer">{comment.commenter.username}</h2>
-
-                <span >{comment.text}</span>
-
+            <section className="comment">
+                <section className="content">
+                    <div className="text">
+                        <h2 onClick={() => navigateToProfile(comment._id)} className="username cursor-pointer">
+                            {comment.by.username}
+                        </h2>
+                        <span>{comment.text}</span>
+                    </div>
+                </section>
+                <section className="details">
+                        <div>{utilService.timeDifferenceUpToWeeks(comment.createdAt)}</div>
+                    <div onClick={() => setModalData({ data: comment.likedBy, dataType: 'likes' })}>
+                        {comment.likedBy.length > 0 &&
+                            `${comment.likedBy.length} ${comment.likedBy.length > 1 ? 'likes' : 'like'}`}
+                    </div>
+                    <div>reply</div>
+                </section>
             </section>
-            <section className="details">
 
-                {/* <div>{utilService.timeDifferenceUpToWeeks(comment.timeStamp)}</div> */}
-
-                <div onClick={() => setModalData({ data: comment.commentLikedBy, dataType: 'likes' })}>
-                    {comment.commentLikedBy.length > 0 &&
-                        `${comment.commentLikedBy.length} ${comment.commentLikedBy.length > 1 ? 'likes' : 'like'}`}
-                </div>
-
-                <div>reply</div>
-
-            </section>
-
-
+            <div className="like">
+                <img
+                    src={postService.isCommentLiked(comment)
+                        ? 'src/imgs/HeartFull.png'
+                        : 'src/assets/svgs/Heart.svg'}
+                    alt=""
+                />
+            </div>
         </section>
-
-
-    </section>
+    )
 }
---
+++ src/cmps/CommentPreviewPlaceholder.jsx
@@ -0,0 +1,6 @@
+export function CommentPreviewPlaceholder() {
+    return <section className="comment-preview-placeholder">
+        <div className="profile"></div>
+        <div className="text"></div>
+    </section>
+}
