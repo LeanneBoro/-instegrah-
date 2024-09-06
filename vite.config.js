1b2ecf0476b0ef1ced324c576ff35977971ddd3f
BenAvrahamos <benavraham1998@gmail.com>
2024-09-06 22:12:10 +0300

    Renamed previous names of project to Instagram

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: '../instegrah backend/public',
+    outDir: '../Instagram backend/public',
     emptyOutDir: true,
   }
 })
